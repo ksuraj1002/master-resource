4aaeae449c540a1863d2c77c63e58f62f6a1c8bb
suraj <ksuraj1002@gmail.com>
2023-02-26 13:03:48 +0530

    i guess now perfect

--- core/src/main/java/com/master/core/constant/BeanConstant.java
+++ core/src/main/java/com/master/core/constant/BeanConstant.java
@@ -1,5 +1,15 @@
 package com.master.core.constant;
 
 public class BeanConstant {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
