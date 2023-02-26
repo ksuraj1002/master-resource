047ff5760c30a7da5e9596904f06ea28d7eaa16c
ksuraj1002 <ksuraj1002@gmail.com>
2023-02-26 22:36:23 +0530

    spring apo

--- feign-client-child/src/main/java/com/feign/client/child/app/service/RegistryService.java
+++ feign-client-child/src/main/java/com/feign/client/child/app/service/RegistryService.java
@@ -1,25 +1,18 @@
 package com.feign.client.child.app.service;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Collection;
 import java.util.List;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.feign.client.child.app.entity.PincodeRegistry;
-import com.feign.client.child.app.repository.PincodeRegistryRepo;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken; 
+import com.feign.client.child.app.repository.PincodeRegistryRepo; 
 
 @Service
 public class RegistryService {
@@ -35,7 +28,7 @@ public class RegistryService {
 	private List<PincodeRegistry> extractPincodeRegistry() throws FileNotFoundException, IOException, ParseException {
 		ObjectMapper mapper = new ObjectMapper();
 		String records = new ObjectMapper()
-				.readTree(Paths.get("H:\\sts-workspace\\master\\src\\pincode_all_India.json").toFile()).get("records")
+				.readTree(Paths.get("H:\\sts-workspace\\master\\src\\pincodes.json").toFile()).get("records")
 				.toPrettyString();
 		List<PincodeRegistry> pincodes=mapper.readValue(records, new TypeReference<List<PincodeRegistry>>() { });
 		
