d5e0b2541db3f1a0c6483871cff331885142aafc
ksuraj1002 <ksuraj1002@gmail.com>
2023-03-25 09:28:25 -0700

    just put

--- feign-client-master/src/main/java/com/feign/client/master/app/FeignClientMasterApplication.java
+++ feign-client-master/src/main/java/com/feign/client/master/app/FeignClientMasterApplication.java
@@ -4,8 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", 
+								description = "Employees Information",contact = @Contact(name="Suraj",email = "sx00@gmail.com",url = "website")),
+					servers = {@Server(url = "serverUrl",description = "prod server")}
+		) 
 public class FeignClientMasterApplication {
 
 	public static void main(String[] args) {
