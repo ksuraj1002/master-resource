569a81048e22a2d97a8784d37123c972682c2e32
ksuraj1002 <ksuraj1002@gmail.com>
2023-03-26 06:44:55 -0700

    push it
    push

--- core/src/main/java/com/master/core/dto/SearchQueryParam.java
+++ core/src/main/java/com/master/core/dto/SearchQueryParam.java
@@ -1,4 +1,12 @@
 package com.master.core.dto;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.Data;
+
+@Data
 public class SearchQueryParam {
+
+	@Parameter
+    private String name;
+
 }
