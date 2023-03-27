a26bd63136320c99cd233d68cd2ec52ac443b713
ksuraj1002 <ksuraj1002@gmail.com>
2023-03-27 08:17:36 -0700

    add param

--- core/src/main/java/com/master/core/dto/SearchQueryParam.java
+++ core/src/main/java/com/master/core/dto/SearchQueryParam.java
@@ -1,12 +1,20 @@
 package com.master.core.dto;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class SearchQueryParam {
 
-	@Parameter
+	@Parameter(name = "name",schema=@Schema(type="string"), required=false, description = "valid source app id", in =ParameterIn.QUERY)
     private String name;
+	
+	@Parameter(name = "notificationType",schema=@Schema(type="string",allowableValues = {"EMAIL","SMS","KAFKA"}), required=false, description = "valid source app id",example = "EMAIL", in =ParameterIn.QUERY)
+	private String notificationType;
+	
+	@Parameter(name = "startDate",schema=@Schema(type="string",format = "date-time"), required=false, description = "valid source app id",example = "EMAIL", in =ParameterIn.QUERY)
+	private String startDate;
 
 }
--- feign-client-master/src/main/java/com/feign/client/master/app/controller/FeignController.java
+++ feign-client-master/src/main/java/com/feign/client/master/app/controller/FeignController.java
@@ -1,10 +1,6 @@
 package com.feign.client.master.app.controller;
 
-import com.master.core.dto.ErrorDetail;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feign.client.master.app.service.FeignService;
+import com.master.core.dto.ErrorDetail;
+import com.master.core.dto.SearchQueryParam;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 public class FeignController {
@@ -28,6 +32,16 @@ public class FeignController {
 	public String getResponse(@PathVariable("msg") String msg) {
 		String data=feingService.getRequestedData(msg);
 		return data;
+	}
+	
+	@GetMapping("/getparam")
+	@Operation(tags = "Notification Service API", summary = "get params",description = "get params",operationId = "toSeeParameterOnSwagger",responses = {
+			@ApiResponse(responseCode = "202",description = "Accepted",content = @Content(array = @ArraySchema(schema=@Schema(implementation = SearchQueryParam.class)),mediaType = MediaType.APPLICATION_JSON_VALUE)),
+			@ApiResponse(responseCode = "400",description = "Bad Reqeust",content = @Content(schema = @Schema(implementation = ErrorDetail.class),mediaType = MediaType.APPLICATION_JSON_VALUE)),
+			@ApiResponse(responseCode = "403",description = "Forbidden",content = @Content(schema = @Schema(implementation = ErrorDetail.class),mediaType = MediaType.APPLICATION_JSON_VALUE)),
+			@ApiResponse(responseCode = "500",description = "Server Error",content = @Content(schema = @Schema(implementation = ErrorDetail.class),mediaType = MediaType.APPLICATION_JSON_VALUE))})
+	public String toSeeParameterOnSwagger(@ParameterObject SearchQueryParam queryParam) {
+		return queryParam.toString();
 	}
 	
 }
